3b087d3f6429502c5e884a14d6080cd13027e772
RoyAl-Vitamin <roalvi95@gmail.com>
2019-01-02 23:32:58 +0500

    В догонку
    Забыл самое важное

--- Lab3_RKG_CRON/src/main/java/vi/al/ro/Main.java
+++ Lab3_RKG_CRON/src/main/java/vi/al/ro/Main.java
@@ -3,9 +3,11 @@ package vi.al.ro;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJms
+@EnableScheduling
 public class Main {
 
     public static void main(String... args) {
